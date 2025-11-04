25688fdb5af635324ed06a67ca140fde11b7ff52
Sajmon_96 <simon8durak@gmail.com>
2025-11-04 18:25:17 +0100

    Finishing App

--- Restaurants.Infrastructure/Repositories/RestaurantsRepository.cs
+++ Restaurants.Infrastructure/Repositories/RestaurantsRepository.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Constants;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 using Restaurants.Infrastructure.Persistence;
+using System.Linq.Expressions;
+using System.Reflection.Metadata;
 
 namespace Restaurants.Infrastructure.Repositories;
 
@@ -16,6 +19,43 @@ public class RestaurantsRepository(RestauranstDbContext dbContext) : IRestaurant
             .ToListAsync();
 
         return restaurants;
+    }
+
+    public async Task<(IEnumerable<Restaurant>, int)> GetAllMatchingAsync(string? searchPhrase, int pageSize, int pageNumber, string? SortBy, SortDirection sortDirection)
+    {
+        string? parameter = searchPhrase?.ToLower();
+
+        var baseQuery = dbContext
+            .Restaurants
+            .AsNoTracking()
+            .Include(x => x.Dishes)
+            .Where(x => parameter == null || (x.Name.Contains(searchPhrase ?? "") || x.Description.Contains(searchPhrase ?? "")));
+
+        var totalCount = await baseQuery.CountAsync();
+
+        if(SortBy != null)
+        {
+            var columnsSelector = new Dictionary<string, Expression<Func<Restaurant, object>>>
+            {
+                { nameof(Restaurant.Name), r => r.Name },
+                { nameof(Restaurant.Description), r => r.Description },
+                { nameof(Restaurant.Category), r => r.Category }
+            };
+
+            var selectedColumn = columnsSelector[SortBy];
+
+
+            baseQuery = sortDirection == SortDirection.Ascending
+                ? baseQuery = baseQuery.OrderBy(selectedColumn)
+                : baseQuery = baseQuery.OrderByDescending(selectedColumn);
+        }
+
+        var restaurant = await baseQuery
+            .Skip(pageSize * (pageNumber - 1))
+            .Take(pageSize)
+            .ToListAsync();
+
+        return (restaurant, totalCount);
     }
 
     public async Task<Restaurant?> GetByIdAsync(int id)
